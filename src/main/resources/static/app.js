93545940cbb9752982fa3cf22e3ce25fe5e92abe
Richard Urrea <richard.urrea@mail.escuelaing.edu.co>
2021-10-12 15:24:44 -0500

    PARTE 3.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,23 +1,27 @@
 var app = (function () {
 
+
+    var idSuscribirse = null;
+
+
     class Point{
         constructor(x,y){
             this.x=x;
             this.y=y;
-        }        
+        }
     }
-    
+
     var stompClient = null;
 
-    var addPointToCanvas = function (point) {        
+    var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
-    
-    
+
+
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
@@ -49,6 +53,8 @@ var app = (function () {
 
 
 
+
+
     return {
 
         init: function () {
@@ -62,7 +68,7 @@ var app = (function () {
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             //publicar el evento
-            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
+            stompClient.send("/topic/newpoint." + idSuscribirse, {}, JSON.stringify(pt));
         },
 
         disconnect: function () {
@@ -71,6 +77,25 @@ var app = (function () {
             }
             setConnected(false);
             console.log("Disconnected");
+        },
+        connectAndSubscribeToOne: function (id) {
+            idSuscribirse = id;
+            console.info('Connecting to WS...');
+            var socket = new SockJS('/stompendpoint');
+            stompClient = Stomp.over(socket);
+
+            //subscribe to /topic/newpoint when connections succeed
+            stompClient.connect({}, function (frame) {
+                console.log('Connected: ' + frame);
+                stompClient.subscribe('/topic/newpoint.' + idSuscribirse, function (message) {
+                    var point=JSON.parse(message.body);
+                    console.log(point);
+                    addPointToCanvas(point);
+                    alert(JSON.stringify(point));
+
+                });
+            });
+
         }
     };
 
